a4ffedc8286a1c3436447efc13a5b48547ebaf7e
Arpan Sarkar <arpan98sarkar@gmail.com>
2021-02-07 17:03:16 +0530

    Challange Ten

--- myApp.js
+++ myApp.js
@@ -87,7 +87,11 @@ app.get("/:word/echo",function(req,res)
 
 /** 10) Get input from client - Query parameters */	
 // /name?first=<firstname>&last=<lastname>	
-
+// app.route(path).get(handler).post(handler) This syntax allows you to chain different verb handlers on the same path route. 
+app.get("/name",function(req,res)
+{
+    res.json({"name": req.query.first + " " + req.query.last});
+});
 
 
 /** 11) Get ready for POST Requests - the `body-parser` */	
