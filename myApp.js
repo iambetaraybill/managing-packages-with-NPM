d34a6818c7db2cbbc3e9aa2bf2ada07b7e833c09
Arpan Sarkar <arpan98sarkar@gmail.com>
2021-02-07 12:49:40 +0530

    Second challange

--- myApp.js
+++ myApp.js
@@ -20,7 +20,9 @@ console.log("Hello World");
 
 /** 2) A first working Express Server */	
 
-
+app.get("/", function(req, res){
+    res.send("Hello Express");
+});
 
 
 /** 3) Serve an HTML file */	
