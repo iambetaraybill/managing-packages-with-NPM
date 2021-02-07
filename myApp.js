a570f79ac124b03f5036f66b1fafbc5bf85634e3
Arpan Sarkar <arpan98sarkar@gmail.com>
2021-02-07 15:44:04 +0530

    Challange Six

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+require('dotenv').config();
 
 
 // --> 7)  Mount the Logger middleware here	
@@ -38,13 +38,22 @@ app.use(express.static(__dirname + "/public"));
 
 /** 5) serve JSON on a specific route */	
 
-app.get("/json",function(req, res){
-    res.json({"message": "Hello json"});
-});
+// app.get("/json",function(req, res){
+//     res.json({"message": "Hello json"});
+// });
 
 
 /** 6) Use the .env file to configure the app */	
+app.get("/json",function(req, res){
+   if(process.env.MESSAGE_STYLE === "uppercase")
+   {
+    res.json({"message": "HELLO JSON"});
+   }else
+  {
+    res.json({"message": "Hello json"});
 
+   }
+});
 
 /** 7) Root-level Middleware - A logger */	
 //  place it before all the routes !	
